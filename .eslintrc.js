8c74fb3643f89c3ef7eb5657d441d1e1ac4c88e1
Ben Teichman <ben@voiceflow.com>
2023-06-07 17:04:26 -0400

    feat: working

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['@voiceflow/eslint-config', '@voiceflow/eslint-config/frontend', '@voiceflow/eslint-config/typescript'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
